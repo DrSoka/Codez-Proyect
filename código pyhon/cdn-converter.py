7402a27c60659827fbdcdaae2b6256afc6ef5d83
Sugarbell <67601252+TaprisSugarbell@users.noreply.github.com>
2021-02-06 19:14:45 -0600

    CDN converter v1.0

--- código pyhon/cdn-converter.py
+++ código pyhon/cdn-converter.py
@@ -1,18 +1,18 @@
 import clipboard
 
+
 print('CDN CONVERTER')
 
 # Variables
 url = input('Ingresa URL: ')
 urlp = url.split('/')
-urlp[2] = 'rawgit.com'
 
 user = urlp[3]
-proyect = urlp[4]
-di = urlp[5:]
-d = '/'.join(di)
+repo = urlp[4]
+di = urlp[6:]
+file = '/'.join(di)
 
-dir = f'https://cdn.jsdelivr.net/gh/{user}/{proyect}@{d}'
+dir = f'https://cdn.jsdelivr.net/gh/{user}/{repo}@{file}'
 
 clipboard.copy(dir)
 print('\nEste es tu link:')
