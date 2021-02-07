eeb5777d252f8edaa37e1a0e80a6ce5c5cef97b1
Sugarbell <67601252+TaprisSugarbell@users.noreply.github.com>
2021-02-06 19:19:19 -0600

    CDN converter v1.1

--- código pyhon/cdn-converter.py
+++ código pyhon/cdn-converter.py
@@ -1,17 +1,13 @@
 import clipboard
 
-
 print('CDN CONVERTER')
-
 # Variables
 url = input('Ingresa URL: ')
 urlp = url.split('/')
-
 user = urlp[3]
 repo = urlp[4]
 di = urlp[6:]
 file = '/'.join(di)
-
 dir = f'https://cdn.jsdelivr.net/gh/{user}/{repo}@{file}'
 
 clipboard.copy(dir)
